dc575b02fc0fd16e579a21f53fae9e489b4f9146
marceloabbadia <marcelo.abbadia@gmail.com>
2023-08-25 16:38:39 -0300

    criando modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,21 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from "../../assets/logo1.png";
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" style={{ width: "120px" }} />
-        <NewTransactionButton>Nova transaçao</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.DialogTrigger asChild>
+            <NewTransactionButton>Nova transaçao</NewTransactionButton>
+          </Dialog.DialogTrigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,43 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+
+export function NewTransactionModal() {
+  return (
+    <Dialog.DialogPortal>
+      <Overlay />
+
+      <Content>
+        <Dialog.Title>Nova Transação</Dialog.Title>
+
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+
+        <form action="">
+          <input type="text" placeholder="Descrição" required />
+          <input type="text" placeholder="Preço" required />
+          <input type="text" placeholder="Categoria" required />
+          <button type="submit">Cadastrar</button>
+
+          <TransactionType>
+            <TransactionTypeButton variant="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome">
+              <ArrowCircleDown size={24} />
+              Saida
+            </TransactionTypeButton>
+          </TransactionType>
+        </form>
+      </Content>
+    </Dialog.DialogPortal>
+  );
+}
