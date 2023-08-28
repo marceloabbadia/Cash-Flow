cee824d3d5570f5ce4141fc1c998174a7aa6bc9c
marceloabbadia <marcelo.abbadia@gmail.com>
2023-08-28 11:41:06 -0300

    criacao de um hook proprio - useSummary

--- src/components/Summery/index.tsx
+++ src/components/Summery/index.tsx
@@ -2,30 +2,11 @@ import { SummaryContainer, SummaryCard } from "./styles";
 import { ArrowCircleUp } from "phosphor-react";
 import { ArrowCircleDown } from "phosphor-react";
 import { CurrencyDollar } from "phosphor-react";
-import { useContext } from "react";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { priceFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext);
-
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type == "income") {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome -= transaction.price;
-        acc.total -= transaction.price;
-      }
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    }
-  );
+  const summary = useSummary();
 
   return (
     <SummaryContainer>
